1c79bda1b4b71665a628b2d3feb8867c1d9b29d6
Konstantin Filimonov <58990191+FilimonovKostya@users.noreply.github.com>
2020-09-07 10:44:12 +0300

    Updates

---
+++ precache-manifest.0e1719e08a22811df0c2dfe84b6e70db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a173adf52e43c11eb31cacc6ac7e58f",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "ab9559091d6e5bf183ab",
+    "url": "/Counter/static/css/main.7f037e24.chunk.css"
+  },
+  {
+    "revision": "cc22c203b1011dc2fd53",
+    "url": "/Counter/static/js/2.56322624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.56322624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9559091d6e5bf183ab",
+    "url": "/Counter/static/js/main.35b9af24.chunk.js"
+  },
+  {
+    "revision": "4a2bea4ebda10b11d1d6",
+    "url": "/Counter/static/js/runtime-main.a4bce0b8.js"
+  }
+]);
