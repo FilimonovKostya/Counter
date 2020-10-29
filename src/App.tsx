ff958415375a530ff79c20973e2065e1e2472670
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-10-29 22:37:49 +0300

    Не ну а почему бы и нет?

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Settings} from "./Components/Block-Settings/Settings";
-import WindowCounter from "./Components/WindowCounter/WindowCounter";
 
 function App() {
-    const [startValue, setStartValue] = useState(0) // значение инпута
-    const [maxValue, setMaxValue] = useState(1) // значение инпута
-    const [count, setCount] = useState(startValue) // сохраняю стартовое значение инпута при нажатии set
-
-
-    const incNumber = () => count < maxValue && setCount(count + 1)
-
-    const resNumber = () => setCount(startValue)
-
-    const settings = () => setCount(startValue)
-
-    console.log(count)
-
-    return <div className={'app'}>
-
-        <Settings setStartValue={setStartValue}
-                  setMaxValue={setMaxValue}
-                  startValue={startValue}
-                  maxValue={maxValue}
-                  settings={settings}
-        />
-
-        <WindowCounter count={count}
-                       startValue={startValue}
-                       maxValue={maxValue}
-                       incNumber={incNumber}
-                       resNumber={resNumber}
-
-        />
-
+    return <div>
+        <h1>test</h1>
     </div>
 }
 
-export default App;
+export default App
