fd2f6da59bddc1f14af5119820d02cb87072e8cc
Filimonov Kostya <konstantinfilimonou@gmail.com>
2020-11-18 21:51:58 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,63 @@
 import React from "react";
 import "./App.css";
-import { BlockSettings } from "./components/BlockSettings";
-import { BlockWindow } from "./components/BlockWindow";
+import {BlockSettings} from "./components/BlockSettings";
+import {BlockWindow} from "./components/BlockWindow";
 
 function App() {
-  const [inputStartValue, setInputStartValue] = React.useState(
-    Number(localStorage.getItem("start"))
-  );
-  const [inputMaxValue, setInputMaxValue] = React.useState(
-    Number(localStorage.getItem("max"))
-  );
+    const [inputStartValue, setInputStartValue] = React.useState(
+        Number(localStorage.getItem("start"))
+    );
+    const [inputMaxValue, setInputMaxValue] = React.useState(
+        Number(localStorage.getItem("max"))
+    );
 
-  const [count, setCount] = React.useState(inputStartValue);
+    const [count, setCount] = React.useState(inputStartValue);
 
-  const [disabled, setDisabled] = React.useState(true);
+    const [disabled, setDisabled] = React.useState(true);
 
-  function inc() {
-    count < inputMaxValue && setCount(count + 1);
+    function inc() {
+        count < inputMaxValue && setCount(count + 1);
 
-    localStorage.setItem("count", count.toString());
-  }
+        localStorage.setItem("count", count.toString());
+    }
 
-  function res() {
-    setCount(inputStartValue);
-  }
+    function res() {
+        setCount(inputStartValue);
+    }
 
-  function set() {
-    setCount(inputStartValue);
-    setDisabled(true);
+    function set() {
+        setCount(inputStartValue);
+        setDisabled(true);
 
-    localStorage.setItem("start", inputStartValue.toString());
-    localStorage.setItem("max", inputMaxValue.toString());
-  }
+        localStorage.setItem("start", inputStartValue.toString());
+        localStorage.setItem("max", inputMaxValue.toString());
+    }
 
-  return (
-    <div className={"App"}>
-      <BlockSettings
-        setInputStartValue={setInputStartValue}
-        setInputMaxValue={setInputMaxValue}
-        startValue={inputStartValue}
-        maxValue={inputMaxValue}
-        set={set}
-        disabled={disabled}
-        setDisabled={setDisabled}
-        count={count}
-      />
+    return (
+        <div className={"App"}>
+            <BlockSettings
+                setInputStartValue={setInputStartValue}
+                setInputMaxValue={setInputMaxValue}
+                startValue={inputStartValue}
+                maxValue={inputMaxValue}
+                set={set}
+                disabled={disabled}
+                setDisabled={setDisabled}
+                count={count}
+            />
 
-      <BlockWindow
-        inc={inc}
-        res={res}
-        inputStartValue={inputStartValue}
-        inputMaxValue={inputMaxValue}
-        count={count}
-        setDisabled={setDisabled}
-        disabled={!disabled}
-        maxValue={inputMaxValue}
-      />
-    </div>
-  );
+            <BlockWindow
+                inc={inc}
+                res={res}
+                inputStartValue={inputStartValue}
+                inputMaxValue={inputMaxValue}
+                count={count}
+                setDisabled={setDisabled}
+                disabled={!disabled}
+                maxValue={inputMaxValue}
+            />
+        </div>
+    );
 }
 
 export default App;
